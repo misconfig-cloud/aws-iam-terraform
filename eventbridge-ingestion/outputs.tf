531828f3a30ea9387c3f4130c35a5dc6f2d58ba4
Yuliyan Tsvetkov <yuliyan.tsvetkov@gmail.com>
2024-10-05 17:03:08 +0300

    Module for EventBridge customer onboarding: v0.0.2

--- eventbridge-ingestion/outputs.tf
+++ eventbridge-ingestion/outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
   description = "The ARN of the IAM role created"
-  value       = aws_iam_role.misconfig-cloud-assume-role.arn
+  value       = aws_iam_role.misconfig-cloud-onboarding-policy.arn
 }
