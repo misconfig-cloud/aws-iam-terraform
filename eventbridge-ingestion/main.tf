fa1df54f49904693cd36292951458893ea30072e
Yuliyan Tsvetkov <yuliyan.tsvetkov@gmail.com>
2024-10-05 18:12:47 +0300

    Module for EventBridge customer onboarding: v1.1.1

--- eventbridge-ingestion/main.tf
+++ eventbridge-ingestion/main.tf
@@ -72,6 +72,38 @@ resource "aws_iam_policy" "misconfig_cloudtrail_policy" {
 EOF
 }
 
+# CloudFormation Policy for the IAM role
+resource "aws_iam_policy" "misconfig_cloudformation_policy" {
+  name   = "Misconfig-CloudFormation-Policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "cloudformation:CreateStack",
+        "cloudformation:UpdateStack",
+        "cloudformation:DeleteStack",
+        "cloudformation:DescribeStacks",
+        "cloudformation:DescribeStackEvents",
+        "cloudformation:DescribeStackResources",
+        "cloudformation:ListStackResources",
+        "cloudformation:ListStacks"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach CloudFormation policy to the role
+resource "aws_iam_role_policy_attachment" "cloudformation_policy_attach" {
+  role       = aws_iam_role.misconfig-cloud-onboarding-policy.name
+  policy_arn = aws_iam_policy.misconfig_cloudformation_policy.arn
+}
+
 # Attach EventBridge policy to the role
 resource "aws_iam_role_policy_attachment" "eventbridge_policy_attach" {
   role       = aws_iam_role.misconfig-cloud-onboarding-policy.name
